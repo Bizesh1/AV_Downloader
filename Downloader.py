b3570895f48dfda83f6d81bfcfe4d61ddd471585
Bizesh1 <shahbijesh1238@gmail.com>
2026-05-23 20:20:08 +0545

    File Added: Video Downloader

--- Downloader.py
+++ Downloader.py
@@ -3,7 +3,7 @@ import yt_dlp as yt
 from abc import ABC, abstractmethod
 
 class Downloader(ABC):
-    def __init__(self, URL, dl_path= "/downloads"):
+    def __init__(self, URL, dl_path):
         self.URL = URL
         self.download_path = dl_path
 
@@ -11,7 +11,8 @@ class Downloader(ABC):
     def build_options(self):  # to be overridden
         options = {
             "quiet" : True,
-            "outtmpl": f"{self.download_path}/%(title)s.%(ext)s",
+            "outtmpl": "%(title)s.%(ext)s",
+            "paths" : {"home":self.download_path},
             "noplaylist" : True
         }
         return options
@@ -19,3 +20,4 @@ class Downloader(ABC):
     def download(self): 
         with yt.YoutubeDL(self.build_options()) as ydl:
             ydl.download([self.URL])
+            print("Download Complete.")
---
+++ VideoDownloader.py
@@ -0,0 +1,24 @@
+import yt_dlp as yt
+from Downloader import Downloader
+
+class VideoDownloader(Downloader):
+    def __init__(self, URL, dl_path="./Downloads/Video_Downloads"):
+        super().__init__(URL, dl_path)
+    
+    def build_options(self):
+        options = super().build_options()
+        options["format"] = "bestvideo+bestaudio"
+        options["merge_output_format"] = "mp4"
+        return options
+
+    def download(self):
+        super().download()
+
+
+# -----Testing-----
+# def main():
+#     A = VideoDownloader("https://www.youtube.com/watch?v=Aq5WXmQQooo")
+#     A.download()
+
+# if __name__ == "__main__":
+#     main()
